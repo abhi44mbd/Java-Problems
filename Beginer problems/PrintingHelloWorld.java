59b7b3627ea0b77365d7d9b5ccba8886efaed552
abhi44mbd <56728042+abhi44mbd@users.noreply.github.com>
2021-10-03 18:18:43 +0530

    First programme

---
+++ Beginer problems/PrintingHelloWorld.java
@@ -0,0 +1,8 @@
+package com.abhi;
+
+public class PrintingHelloWorld {
+    public static void main(String args[]){
+        //to print use system.out
+        System.out.println("Hello world ! ");
+    }
+}
