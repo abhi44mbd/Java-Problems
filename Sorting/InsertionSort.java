f684b5ec090d56b56f4ff87798baec9de2092575
Abhishek Kumar <56728042+abhi44mbd@users.noreply.github.com>
2022-01-22 20:45:35 +0530

    Update InsertionSort.java

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -5,14 +5,14 @@ public class InsertionSort {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number of elements you want to store: ");
-//reading the number of elements from the that we want to enter
+        //reading the number of elements from the that we want to enter
         int n = sc.nextInt();
-//creates an array in the memory of length 10
+        //creates an array in the memory of length 10
         int[] array = new int[n];
         System.out.println("Enter the elements of the array: ");
         for(int i=0; i<n; i++)
         {
-//reading array elements from the user
+        //reading array elements from the user
             array[i]=sc.nextInt();
         }
         Insertion(array);
